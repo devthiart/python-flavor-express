1aa4c9ddfbdf91e1949ab09dbb9912e8df7e7526
Thiago Andre <devthiart@gmail.com>
2024-03-04 16:31:11 -0300

    Good Habits - Snake case

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ print('##### Flavor Express #####\n')
 print('1. Register restaurant')
 print('2. List restaurant')
 print('3. Active restaurant')
-print('4. Exit restaurant')
+print('4. Exit')
+
+chosen_option = input('Choose an option: ')
+
+print('You chose the option ', chosen_option)
