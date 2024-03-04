ab5e924501c4a30ea6ae5505cfe04c67c9b94cc2
Thiago Andre <devthiart@gmail.com>
2024-03-04 16:10:55 -0300

    First Program

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print('Hello World')
+print('##### Flavor Express #####\n')
+
+print('1. Register restaurant')
+print('2. List restaurant')
+print('3. Active restaurant')
+print('4. Exit restaurant')
